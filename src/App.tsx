f53368559ff4cedff5042220714204f7124ea7fc
Kathleen <99849068+Kathleen98@users.noreply.github.com>
2025-02-10 10:56:17 +0000

    feat: Starting home page styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/Theme";
 import { GlobalStyle } from "./styles/Global";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./components/Route";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div></div>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );

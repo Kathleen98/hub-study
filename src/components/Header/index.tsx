74d19c92f815743230d9594000ea008fd99c4b69
Kathleen <99849068+Kathleen98@users.noreply.github.com>
2025-02-10 12:29:45 +0000

    feat: adding default application layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,26 @@
+import { Scroll, Timer } from "phosphor-react";
 import { HeaderContainer } from "./style";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  return <HeaderContainer>Header</HeaderContainer>;
+  return (
+    <HeaderContainer>
+      <img
+        src={
+          "https://github.com/user-attachments/assets/8f138b13-7a33-4547-a2ed-81829c611e2c"
+        }
+      />
+
+      <nav>
+        <NavLink to={"/"} title={"timer"}>
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to={"/history"} title={"histórico"}>
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  );
 };
 
 export default Header;
